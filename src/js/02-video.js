be8f6eaceb71880b55b93302c056020ba5925a80
OlegKushnir <kushnir.oleg87@gmail.com>
2022-09-26 15:11:43 +0300

    update
    task1
    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';   
+import throttle from 'lodash.throttle';
+
+const savedSettings = localStorage.getItem("videoplayer-current-time");
+const iframe = document.querySelector('iframe');
+        const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+if (savedSettings) {
+    const parsedSettings = JSON.parse(savedSettings);
+    player.setCurrentTime(parsedSettings).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+}
+
+function onTimeupdate ({seconds}) {
+    localStorage.setItem("videoplayer-current-time", seconds);
+};
 
